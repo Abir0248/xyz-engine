d8eefdd3e2534b3b43c71bcb23fdbfae17318275
Abir0248 <abir15-6789@diu.edu.bd>
2023-08-18 04:28:49 +0600

    task 1 step 1

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Step1 from "./components/Step1";
+// import Step2 from "./components/Step2";
+// import Result from "./components/Result";
+import "./index.css";
+
+function App() {
+  // const [formData, setFormData] = useState({
+  //   projectName: "",
+  //   projectDescription: "",
+  //   client: "",
+  //   contractor: "",
+  //   csvFile: null,
+  //   maxX: 0,
+  //   minX: 0,
+  //   maxY: 0,
+  //   minY: 0,
+  //   maxZ: 0,
+  //   minZ: 0,
+  // });
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Step1 />} />
+        {/* <Route path="/step-2" element={<Step2 />} /> */}
+        {/* <Route path="/result" element={<Result />} /> */}
+        {/* <Route path="/" element={<Step1 setFormData={setFormData} />} /> */}
+        {/* <Route path="/step-2" element={<Step2 formData={formData} />} /> */}
+        {/* <Route path="/result" element={<Result formData={formData} />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        burlywood: "#DEB887",
+        lightgray: "#F8F8F8",
+        offwhite: "#FCFCFC",
+      },
+    },
+  },
+  plugins: [],
+};
